4d948b7d6e1524e10e5d1fbc9d7bd51e647ebc9f
Pablo <arturo3393@gmail.com>
2023-03-09 17:33:44 -0500

    Project structure added and JPA with entities and their map added

---
+++ src/main/java/com/pablo/market/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,26 @@
+package com.pablo.market.persistence.entity;
+
+import jakarta.persistence.Column;
+
+public class ComprasProductoPK {
+    @Column(name = "id_compra")
+    private Integer idCompra;
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+}
---
+++ src/test/java/com/pablo/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fe6600ecc1f5e940101a424874c0ef5d39e56b1f
Pablo <arturo3393@gmail.com>
2023-03-13 17:47:58 -0500

    orienting the repository to the domain

--- src/main/java/com/pablo/market/persistence/ProductoRepository.java
+++ src/main/java/com/pablo/market/persistence/ProductoRepository.java
@@ -1,33 +1,54 @@
 package com.pablo.market.persistence;
 
+import com.pablo.market.domain.Product;
+import com.pablo.market.domain.respository.ProductRepository;
 import com.pablo.market.persistence.crud.ProductoCrudRepository;
 import com.pablo.market.persistence.entity.Producto;
+import com.pablo.market.persistence.mapper.ProductMapper;
+import org.springframework.expression.spel.ast.OpPlus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
 
     private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
+    @Override
+    public List<Product> getAll(){
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
+    }
 
-    public List<Producto> getAll(){
-        return (List<Producto>) productoCrudRepository.findAll();
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos =  productoCrudRepository.findbyIdCategoriaOrderByNombreASC(categoryId);
+        return Optional.of(mapper.toProducts(productos));
+
+    }
+
+    @Override
+    public Optional<List<Product>> getScarceProducts(int quantity) {
+        Optional<List<Producto>> productos =productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        Optional<U> u = productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+        return u;
+    }
+    @Override
+    public Product save(Product product) {
+        return null;
     }
 
     //Calling the query methods
-    public List<Producto> getByCategoria (int idCategoria){
-        return productoCrudRepository.findbyIdCategoriaOrderByNombreASC(idCategoria);
-    }
 
-    public Optional<List<Producto>> getEscasos(int cantidad){
-        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
-    }
 
-    public Optional<Producto> getProducto(int idProducto){
-        return productoCrudRepository.findById(idProducto);
-    }
+
 
     public Producto save (Producto producto){
         return productoCrudRepository.save(producto);
